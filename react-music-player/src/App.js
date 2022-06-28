7f1d5209fb0668a3ebf4dccf14a3b75f3f7f9c93
Theo <theodorosladasmail@gmail.com>
2022-06-28 21:35:55 +0100

    created - components

--- react-music-player/src/App.js
+++ react-music-player/src/App.js
@@ -1,7 +1,14 @@
 import React from "react";
+import Song from "./components/Song";
+import Player from "./components/Player";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Song />
+      <Player />
+    </div>
+  );
 }
 
 export default App;
