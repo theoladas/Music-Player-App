9d8ead413a469f2d9471b632164a47550202dc8f
Theo <theodorosladasmail@gmail.com>
2022-06-29 12:17:10 +0100

    added - song state and pass prop to Song.js

--- react-music-player/src/App.js
+++ react-music-player/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 // import styles
 import "./styles/app.scss";
 // import components
@@ -8,9 +8,14 @@ import Player from "./components/Player";
 import data from "./data";
 
 function App() {
+  // create state
+  const [songs, setSongs] = useState(data());
+  // we need to grab the 1st song of the songs array, as the current song
+  const [currentSong, setCurrentSong] = useState(songs[0]);
   return (
     <div className="App">
-      <Song />
+      {/* pass the currentSong to the Song component */}
+      <Song currentSong={currentSong} />
       <Player />
     </div>
   );
