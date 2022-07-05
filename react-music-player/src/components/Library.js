e052c712efad928c436621946e1ae9e43ee38bb5
Theo <theodorosladasmail@gmail.com>
2022-07-05 19:25:55 +0100

    Added - loop over each song and pass song's information to LibrarySong

--- react-music-player/src/components/Library.js
+++ react-music-player/src/components/Library.js
@@ -5,7 +5,13 @@ const Library = ({ songs }) => {
   return (
     <div className="library">
       <h2>Library</h2>
-      <div className="library-songs">{/* <LibrarySong songs={songs} /> */}</div>
+      <div className="library-songs">
+        {/* From all songs (from data.js) we grab each song, we map over them and create a component for each one. */}
+        {songs.map((song) => (
+          // pass all the information for the song to the component
+          <LibrarySong song={song} />
+        ))}
+      </div>
     </div>
   );
 };
