c100be27e29aae7329347e7eef8f2265e4b9e7f6
Theo <theodorosladasmail@gmail.com>
2022-07-14 22:01:06 +0100

    Updated - cut the promise code form LibraySongs and paste it to Util.js

--- react-music-player/src/components/LibrarySong.js
+++ react-music-player/src/components/LibrarySong.js
@@ -32,20 +32,6 @@ const LibrarySong = ({
     });
     // update the state:
     setSongs(newSongs);
-
-    // check if the song is playing:
-    if (isPlaying) {
-      // create a play promise:
-      const playPromise = audioRef.current.play();
-      // if the audio is undefined (hasnt loaded yet):
-      if (playPromise !== undefined) {
-        playPromise.then((audio) => {
-          audioRef.current.play();
-        });
-      }
-    }
-    // make the song play when we click it from the Library
-    // audioRef.current.play();
   };
 
   return (
